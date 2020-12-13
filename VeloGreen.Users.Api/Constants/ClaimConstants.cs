2ad4cf9895542e0e37997de0473fa60dc5644277
Patric Green <bagro@bagro.se>
2020-12-13 17:20:23 +0100

    Add claims constants

---
+++ VeloGreen.Users.Api/Constants/ClaimConstants.cs
@@ -0,0 +1,11 @@
+namespace VeloGreen.Users.Api.Constants
+{
+    public class ClaimConstants
+    {
+        public const string Email = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress";
+        
+        public const string NameIdentifier = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier";
+
+        public const string Admin = "Admin";
+    }
+}
